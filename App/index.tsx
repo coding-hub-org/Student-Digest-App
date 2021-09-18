960b32b0b12b0f12b28959df245f5da860782ffc
NelsonTejeda <nelsonjesustejeda@gmail.com>
2021-09-18 17:13:02 -0400

    project set-up

--- App/index.tsx
+++ App/index.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {TextCom} from './components/textComponent'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextCom/>
       <StatusBar style="auto" />
     </View>
   );
