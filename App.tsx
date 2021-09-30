ab19209ecb0ae8a1f541868681efa27938db566f
Emonestime308@gmail.com <Eric monestime>
2021-09-30 19:55:24 -0400

    nav bar button

--- App.tsx
+++ App.tsx
@@ -2,13 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextCom } from './App/components/textComponent';
+import {NavigationContainer} from '@react-navigation/native';
+import Tabs from './App/components/tabs';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TextCom/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs/>
+    </NavigationContainer>
+
+    
   );
 }
 
