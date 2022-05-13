8fbe82d0e155f4b899390cb847ed5ce4d155732c
NelsonTejeda <nelsonjesustejeda@gmail.com>
2022-05-13 19:41:00 -0400

    finished

--- App/screens/aboutScreen.js
+++ App/screens/aboutScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
+import { List } from "react-native-paper";
 
 export const AboutScreen = () => {
   //add your buttons in the view
@@ -9,22 +10,34 @@ export const AboutScreen = () => {
         style={styles.tinyLogo}
         source={require("../../assets/SDACOVER.png")}
       />
-      <Text>
-        The Student Digest App is Plattsburgh's most-read news app on SUNY
-        Plattsburgh Campus. More than 500 on campus students wake up new digest
-        every morning. Published since ##YEAR, the digest has shown no signs of
-        aging and keeps reinventing itself to remain in step with its young
-        readership. Known as a trend-setter that adheres to the highest
-        standards of journalism, Student Digest not only provides news coverage
-        of unrivalled depth and breadth but also places equal, often greater,
-        emphasis on perspective and context to empower the reader. Its coverage
-        includes every topic and trend of interest to the reader - national,
-        international, city, community, business, health, science, technology,
-        sports, lifestyle and entertainment. Instead of taking the traditional
-        role of newspapers to "mould public opinion", The Times of India
-        provides the choices that enable readers to arrive at their own
-        conclusions. The reader is the centre of its universe.
-      </Text>
+      <ScrollView>
+        <Text style={styles.textStyle}>
+          The Student Digest App is Plattsburgh's most-read news app on SUNY
+          Plattsburgh Campus. More than 500 students wake up to new digest every
+          morning. The digest has shown no signs of aging and keeps reinventing
+          itself to remain in step with its young readership. Known as a
+          trend-setter that adheres to the highest standards of journalism,
+          Student Digest not only provides news coverage of unrivalled depth and
+          breadth but also places equal, often greater, emphasis on perspective
+          and context to empower the reader. Its coverage includes every topic
+          and trend of interest to the reader - national, international, city,
+          community, business, health, science, technology, sports, lifestyle
+          and entertainment. The Student Digest App was made to help students
+          access this information as swiftly as possible.
+        </Text>
+        <List.AccordionGroup>
+          <List.Accordion title="Meet the team!" id="1">
+            <List.Item title="Nelson Tejeda" />
+            <List.Item title="Christopher Ovalle" />
+            <List.Item title="Jason Zheng" />
+            <List.Item title="Amornrat Ajmo" />
+            <List.Item title="Joey Silvanus" />
+            <List.Item title="Aditya Thakkar " />
+            <List.Item title="Manasi Jadhav" />
+            <List.Item title="Pratyush Ketan Kapadia" style={styles.dropDown} />
+          </List.Accordion>
+        </List.AccordionGroup>
+      </ScrollView>
     </View>
   );
 };
@@ -32,6 +45,19 @@ export const AboutScreen = () => {
 const styles = StyleSheet.create({
   tinyLogo: {
     width: "100%",
-    height: "40%",
+    height: "30%",
+  },
+  textStyle: {
+    margin: "2%",
+    padding: "4%",
+    backgroundColor: "white",
+    borderRadius: 30,
+    textAlign: "auto",
+  },
+  items: {
+    margin: "5%",
+  },
+  dropDown: {
+    marginBottom: "100%",
   },
 });
--- App/screens/askedQuestion.js
+++ App/screens/askedQuestion.js
@@ -1,19 +1,57 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { List } from "react-native-paper";
 
 export const FrequentlyAskedQuestions = () => {
   //add your buttons in the view
   return (
     <View>
-      <Text>
-        Who made the app?: Students from the Coding-Hub club and tech start-up
-        class have helped developed this app Where does the information come
-        from?: The student digest Where do my digest get saved?: All digest are
-        saved locally on your device. Why do the photos sometimes don't make
-        sense?: All photos are chosen based on the title of the digest, and
-        pulled from an image database. The image database chooses the best
-        option for the context.
-      </Text>
+      <List.AccordionGroup>
+        <List.Accordion title="Who made the app?" id="1">
+          <Text style={styles.items}>
+            Students from the Coding-Hub club and tech start-up class have
+            helped developed this app
+          </Text>
+        </List.Accordion>
+        <List.Accordion
+          title="Where does the information come
+        from?"
+          id="2"
+        >
+          <Text style={styles.items}>
+            All digest information comes directly from the Student Digest
+            website.
+          </Text>
+        </List.Accordion>
+        <List.Accordion title="Where do my digest get saved?" id="3">
+          <Text style={styles.items}>
+            All digest are saved locally on your device.
+          </Text>
+        </List.Accordion>
+        <List.Accordion
+          title="Why do some photos have no correlation to certain digest"
+          id="4"
+        >
+          <Text style={styles.heading}>
+            Why do some photos have no correlation to certain digest
+          </Text>
+          <Text style={styles.items}>
+            All photos are chosen based on the title of the digest, and pulled
+            from an image database. The image database chooses the best option
+            for the context.
+          </Text>
+        </List.Accordion>
+      </List.AccordionGroup>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  heading: {
+    margin: "5%",
+    fontWeight: "bold",
+  },
+  items: {
+    margin: "5%",
+  },
+});
